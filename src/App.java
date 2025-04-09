8a949d4820b066d45eac429ae4b1b2d345d33b3f
Emerson <emersonnjunior2006@gmail.com>
2025-04-08 21:17:06 -0300

    aula de java 10

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import model.*;
+import controller.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        model.App.main(args);
+        App.main(args);
+    }
+}
